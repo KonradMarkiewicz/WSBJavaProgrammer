46d01d73922476f2cfb3778dbb458a850b284d19
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2020-11-26 20:32:00 +0100

    Java - zadanie 2 - get a car

--- src/com/wsb/Main.java
+++ src/com/wsb/Main.java
@@ -4,12 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal pet = new Animal("cat");
+        Human human = new Human();
 
+        Animal pet = new Animal("cat");
+        human.pet = pet;
         while (pet.isAlive) {
             pet.takeForAWalk();
             pet.feed(0.5);
         }
 
+        Car fiat = new Car(2003, "Fiat", "Bravo", "gray", 10000.0);
+        System.out.println("\n" + fiat.producer + " " + fiat.model + " " + fiat.yearOfProduction + "\n" + fiat.color + " " + fiat.value + " PLN");
+        human.car = fiat;
     }
 }
